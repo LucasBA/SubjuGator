03c150865aef01dfa6dc2f1e5dda666c10e8f82c
Lucas Bassett-Audain <lbassettaudain@ufl.edu>
2018-08-07 14:35:08 -0400

    UKF: Odom lines up with bag

--- command/sub8_launch/scripts/state_message_remapper.py
+++ command/sub8_launch/scripts/state_message_remapper.py
@@ -1,50 +1,12 @@
 #!/usr/bin/env python
 import rospy
+import sys
+import numpy as np
 import tf.transformations
 from mil_msgs.msg import DepthStamped, VelocityMeasurements, PoseTwistStamped
 from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import PoseWithCovarianceStamped, Twist, TwistStamped, TwistWithCovarianceStamped
-
-
-def got_depth(msg):
-    depth_pub = rospy.Publisher('/ukf_depth', PoseWithCovarianceStamped, queue_size=1)
-    p = PoseWithCovarianceStamped()
-    p.header = msg.header
-    # Create a new pose
-    p.pose.pose.position.x = 0.0
-    p.pose.pose.position.y = 0.0
-    p.pose.pose.position.z = -msg.depth
-    depth_pub.publish(p)
-
-
-def got_mag(msg):
-    mag_pub = rospy.Publisher('/ukf_mag', PoseWithCovarianceStamped, queue_size=1)
-    p = PoseWithCovarianceStamped()
-    p.header.stamp = rospy.Time.now()
-    p.header.frame_id = 'imu'
-    quat = tf.transformations.quaternion_from_euler(msg.x, msg.y, msg.z)
-    p.pose.pose.orientation.x = quat[0]
-    p.pose.pose.orientation.y = quat[1]
-    p.pose.pose.orientation.z = quat[2]
-    p.pose.pose.orientation.w = quat[3]
-    mag_pub.publish(p)
-
-
-def got_dvl(msg):
-    dvl_pub = rospy.Publisher('/ukf_dvl', TwistWithCovarianceStamped, queue_size=1)
-    v = TwistWithCovarianceStamped()
-    v.header = msg.header
-    new_twist = dvl2twist(msg.velocity_measurements)
-    v.twist.twist = new_twist
-    dvl_pub.publish(v)
-
-
-def got_trajectory(msg):
-    trajectory_pub = rospy.Publisher('/cmd_vel', TwistStamped, queue_size=1)
-    v = TwistStamped()
-    v.header = msg.header
-    v.twist = msg.posetwist.twist
-    trajectory_pub.publish(v)
+from nav_msgs.msg import Odometry
 
 
 def dvl2twist(velocity_measurements):
@@ -60,11 +22,79 @@ def dvl2twist(velocity_measurements):
     return new_twist
 
 
-if __name__ == '__main__':
-    rospy.init_node('state_message_remaper')
-    depth_sub = rospy.Subscriber('/depth', DepthStamped, got_depth)
-    mag_sub = rospy.Subscriber('/mag_orientation', Vector3, got_mag)
-    dvl_sub = rospy.Subscriber('/dvl', VelocityMeasurements, got_dvl)
-    trajectory_sub = rospy.Subscriber('/trajectory', PoseTwistStamped, got_trajectory)
+class Remapper:
 
+    def __init__(self):
+        rospy.sleep(1.0)
+        self.initial_odom_check = True
+        self.depth_sub = rospy.Subscriber('/depth', DepthStamped, self.got_depth)
+        self.mag_sub = rospy.Subscriber('/mag_orientation', Vector3, self.got_mag)
+        self.dvl_sub = rospy.Subscriber('/dvl', VelocityMeasurements, self.got_dvl)
+        self.trajectory_sub = rospy.Subscriber('/trajectory', PoseTwistStamped, self.got_trajectory)
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.got_odom)
+        self.initial_odom_sub = rospy.Subscriber('/initial_odom', Odometry, self.got_initial_odom)
+
+
+    def got_depth(self,msg):
+        depth_pub = rospy.Publisher('/ukf_depth', PoseWithCovarianceStamped, queue_size=1)
+        p = PoseWithCovarianceStamped()
+        p.header = msg.header
+        p.header.frame_id = "map"
+        # Create a new pose
+        p.pose.pose.position.x = 0.0
+        p.pose.pose.position.y = 0.0
+        p.pose.pose.position.z = -msg.depth
+        p.pose.covariance = np.zeros(36) #+ 1e-2
+        depth_pub.publish(p)
+
+
+    def got_mag(self,msg):
+        mag_pub = rospy.Publisher('/ukf_mag', PoseWithCovarianceStamped, queue_size=1)
+        p = PoseWithCovarianceStamped()
+        p.header.stamp = rospy.Time.now()
+        p.header.frame_id = 'imu'
+        quat = tf.transformations.quaternion_from_euler(msg.x, msg.y, msg.z)
+        p.pose.pose.orientation.x = quat[0]
+        p.pose.pose.orientation.y = quat[1]
+        p.pose.pose.orientation.z = quat[2]
+        p.pose.pose.orientation.w = quat[3]
+        mag_pub.publish(p)
+
+
+    def got_dvl(self,msg):
+        dvl_pub = rospy.Publisher('/ukf_dvl', TwistWithCovarianceStamped, queue_size=1)
+        v = TwistWithCovarianceStamped()
+        v.header = msg.header
+        new_twist = dvl2twist(msg.velocity_measurements)
+        v.twist.twist = new_twist
+        dvl_pub.publish(v)
+
+
+    def got_trajectory(self,msg):
+        trajectory_pub = rospy.Publisher('/cmd_vel', TwistStamped, queue_size=1)
+        v = TwistStamped()
+        v.header = msg.header
+        v.twist = msg.posetwist.twist
+        trajectory_pub.publish(v)
+
+
+    def got_odom(self, msg):
+        odom_pub = rospy.Publisher('/initial_odom', Odometry, queue_size=1)
+        if (self.initial_odom_check):
+            print "~~~~~~~~~~~~~~~~~~~PUBLISH ODOM~~~~~~~~~~~~~~~~~~"
+            msg.pose.covariance = np.zeros(36)
+            odom_pub.publish(msg)
+            rospy.sleep(1.0)
+
+
+    def got_initial_odom(self, msg):
+        self.initial_odom_check = False
+
+def main(args):
+    Remapper()
     rospy.spin()
+
+if __name__ == '__main__':
+    print "~~~~~~~~~~~~~~~~~~~~~~~~REMAPPER~~~~~~~~~~~~~~~~~~~~~~~"
+    rospy.init_node('state_message_remaper')
+    main(sys.argv)
